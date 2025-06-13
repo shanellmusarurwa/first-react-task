769b836598caa478fd753a88227a8693c5683a7f
Mitchelle <shanellmusarurwa6@gmail.com>
2025-06-13 16:51:52 +0200

    updated some packages

--- react task one/src/App.jsx
+++ react task one/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React from 'react';
+import './App.css';
+import ProfileCard from './components/ProfileCard';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>My Profile</h1>
+      <ProfileCard
+        name="Mitchelle"
+        image="/Public/mitchelle.jpg"
+        bio="I'm a creative and driven web developer with a passion for building responsive, user-friendly websites and applications. I specialize in front-end development using HTML, CSS, JavaScript, and Bootstrap, and I’m experienced in backend development with Node.js, Express, and MySQL."
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;

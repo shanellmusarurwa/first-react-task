6f7cdb2a47e352699c305bb6c9d5c4bc1ed95641
Mitchelle <shanellmusarurwa6@gmail.com>
2025-06-13 18:32:24 +0200

    changed my image path

--- react task one/src/App.jsx
+++ react task one/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <h1>My Profile</h1>
       <ProfileCard
         name="Mitchelle"
-        image="../Public/mitchelle.jpg"
+        image={process.env.PUBLIC_URL + '/mitchelle.jpg'}
         bio="I'm a creative and driven web developer with a passion for building responsive, user-friendly websites and applications. I specialize in front-end development using HTML, CSS, JavaScript, and Bootstrap, and I’m experienced in backend development with Node.js, Express, and MySQL."
       />
     </div>
